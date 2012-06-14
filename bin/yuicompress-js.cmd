b82c39e60c16b6c8c01c2028d0e6d53af8f15f93
kmashint <kmashint@yahoo.com>
2012-06-13 22:52:22 -0400

    fix minify

--- bin/yuicompress-js.cmd
+++ bin/yuicompress-js.cmd
@@ -10,10 +10,10 @@ pushd ..\src
 
 set file=..\web\common\akme-core
 @echo off
-echo>"%file%" // %file%
+echo>"%file%.src.js" // %file%
 for %%a in (akme-core.js akme-context.js akme-dom.js akme-more.js akme-storage.js) ^
 do type>>"%file%.src.js" "%%a"
-%yuic% >>"%file%.min.js" "%file%.src.js"
+%yuic% >"%file%.min.js" "%file%.src.js"
 @echo on
 
 popd
