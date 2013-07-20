b1dbda2448c3b7ef5fdf77b06295f2d7641278cd
kmashint <kmashint@yahoo.com>
2013-07-20 12:48:11 -0400

    improve Context parent handling

--- web/common/akme-core.src.js
+++ web/common/akme-core.src.js
@@ -853,7 +853,7 @@ if (!this.akme) this.akme = {
 	}
 	
 })(akme,"akme.core.Promise");
-// akme.getContext, akme.App
+// akme.getContext
 // Javascript Types: undefined, null, boolean, number, string, function, or object; Date and Array are typeof object.
 // Javascript typeof works for a function or object but cannot always be trusted, e.g. typeof String(1) is string but typeof new String(1) is object.
 // instanceof is better, but will not work between frames/windows/js-security-contexts due to different underlying prototypes.
@@ -875,28 +875,38 @@ if (!this.akme) this.akme = {
 	//
 	// Initialise instance and public functions
 	//
-	function Context(parent) {
-		var p = { parent: parent || null, map: {}, count: 0, refreshDate: null };
+	function Context(parent, refreshFnOrHandleEventOb) {
+		if (!(parent instanceof Context)) parent = null;
+		var p = { parent: parent, map: {}, count: 0, refreshDate: null };
 		this.PRIVATES = function(self){ return self === PRIVATES ? p : undefined; };
+		
 		$.core.EventSource.apply(this); // Apply/inject/mixin event handling.
+		var self = this;
 		this.onEvent("refresh", function(ev) {
 			p.refreshDate = new Date();
+			if (typeof refreshFnOrHandleEventOb === "function") {
+				$.handleEvent(refreshFnOrHandleEventOb, ev);
+			}
 		});
 		this.refresh();
+		if (parent) $.setProperty($.THIS, "akme.getContext", function() {
+			return self;
+		});
 	}
 	$.extend($.copyAll( // class constructor
-		Context, {CLASS: CLASS}
+		Context, {CLASS: CLASS, getRootContext: getRootContext}
 	),{ // static prototype
+		isFunction: isFunction,
 		has: has,
 		get: get,
 		set: set,
 		remove: remove,
+		refresh: refresh,
+		destroy: destroy,
 		getIdCount: getIdCount,
 		getIdArray: getIdArray,
 		getParent: getParent,
 		getRefreshDate: getRefreshDate,
-		isFunction: isFunction,
-		refresh: refresh
 	});
 	$.setProperty($.THIS, CLASS, Context);
 	
@@ -910,10 +920,26 @@ if (!this.akme) this.akme = {
 	//
 	
 	/**
+	 * Get the ROOT Context.
+	 */
+	function getRootContext() {
+		return CONTEXT;
+	}
+	
+	/**
 	 * Refresh the context, also called during initialisation.
 	 */
 	function refresh() {
 		this.doEvent({ type:"refresh", context:this });
+	}
+	
+	function destroy() {
+		var p = this.PRIVATES(PRIVATES), parent = p.parent;
+		this.doEvent({ type:"destroy", context:this });
+		for (var id in p.map) this.remove(id); 
+		if (parent) $.setProperty($.THIS, "akme.getContext", function() {
+			return parent;
+		});
 	}
 	
 	/**
@@ -931,19 +957,30 @@ if (!this.akme) this.akme = {
 	}
 	
 	/**
+	 * Check if the item at the given id is a function/constructor as opposed to an object/instance.
+	 * This also checks the parent.
+	 */
+	function isFunction(id) {
+		return typeof this.get(id) === "function";
+	}
+
+	/**
 	 * Check for the object/instance at the given key/id, returning true/false.
-	 * This does not fire any "has" event.
+	 * This does not fire any "has" event, and will check the parent.
 	 */
 	function has(id) {
-		return (id in this.PRIVATES(PRIVATES).map);
+		var p = this.PRIVATES(PRIVATES);
+		return (id in p.map || (p.parent && p.parent.has(id)));
 	}
 	
 	/**
 	 * Get the object/instance at the given key/id or null.
-	 * Will NOT return undefined.
+	 * Will NOT return undefined, and will check the parent.
 	 */
 	function get(id) {
-		var o = this.PRIVATES(PRIVATES).map[id]; 
+		var p = this.PRIVATES(PRIVATES);
+		var o = p.map[id];
+		if (o === undefined && p.parent) o = p.parent.get(id);  
 		if (typeof o === "function") o = $.newApplyArgs(o, Array.prototype.slice.call(arguments, 1));
 		if (o === undefined) o = null;
 		this.doEvent({ type:"get", context:this, id:id, instance:o });
@@ -952,6 +989,7 @@ if (!this.akme) this.akme = {
 	
 	/**
 	 * Set the given object/instance to the given key/id, returning any existing one or null.
+	 * This does NOT affect the parent.
 	 */
 	function set(id, instance) {
 		var p = this.PRIVATES(PRIVATES), map = p.map;
@@ -964,6 +1002,7 @@ if (!this.akme) this.akme = {
 
 	/**
 	 * Removes the instance at the given id, returning the existing one.
+	 * This does NOT affect the parent.
 	 */
 	function remove(id) {
 		var p = this.PRIVATES(PRIVATES), map = p.map;
@@ -974,21 +1013,20 @@ if (!this.akme) this.akme = {
 		return old;
 	}
 
+	/**
+	 * Get the count of id/key items in this Context map, not including the parent.
+	 */
 	function getIdCount() {
 		return this.PRIVATES(PRIVATES).count;
 	}
 
+	/**
+	 * Get an Array of id/key items in this Context map, not including the parent.
+	 */
 	function getIdArray() {
 		var a=[], i=0;
 		for (key in this.PRIVATES(PRIVATES).map) a[i++] = key;
 		return a;
-	}
-
-	/**
-	 * Check if the item at the given id is a function/constructor as opposed to an object/instance.
-	 */
-	function isFunction(id) {
-		return typeof this.PRIVATES(PRIVATES).map[id] === "function";
 	}
 
 })(akme, "akme.core.Context");
