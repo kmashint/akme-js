b2ba85e5f4972a5075f1702783b7841262f2ce7a
kmashint <kmashint@yahoo.com>
2014-04-30 23:23:41 -0400

    console-sender

---
+++ web/common/console-sender.js
@@ -0,0 +1,213 @@
+// console-sender.js - send console.log/info/warn/error events to console.remoteURL
+//
+
+/**
+ * Buffer console.log/info/warn/error and send remotely if so configured.
+ * If remoteURL is configured only error level logs will be sent.
+ * If remoteLevel is configure only those level logs or higher will be sent {"log":1,"info":3,"warn":4,"error":5}.
+ * MSIE/Trident, even 10, does not support console.debug.
+ * 
+ * TODO: Only provide Authorization once as a re-try, assuming *_token cookie will handle it thereafter.
+ * TODO: If even the AUthorization re-try fails, purge the logs to be sent.
+ * TODO: Encapsulate behind a function, console.akme() / console.akme({}) style, but still publish *Local methods.
+ * 
+ */
+(function($,console){
+	if (!console || console.logLocal) return; // One-time.
+
+	//
+	// private closure variables 
+	//
+	var	//"debug":2, //console.debug not in MSIE10
+		LOG_EVENTS = {"log":1,"info":3,"warn":4,"error":5}, 
+		SEND_TIMEOUT = 5*1000,
+		RECV_TIMEOUT = 45*1000,
+		CHECK_TIMEOUT = 15*60*1000,
+		MIME_TYPE = "application/json; charset=iso-8859-1",
+		tridentPos = navigator.userAgent.indexOf(" Trident/"),
+		isIE = tridentPos != -1,
+		isIE9 = / Trident\/[45]\./.test(navigator.userAgent.substring(tridentPos,tridentPos+11)),
+		timer = null,
+		storage = $.sessionStorage,
+		itemType = "console",
+		NAMES = {
+			"localLevel":1,
+			"remoteURL":1,
+			"remoteLevel":1,
+			"remoteAttemptDate":1,
+			"remoteSuccessDate":1 };
+	
+	//
+	// public constructor/injector/singleton
+	//
+	self = {
+			sendTimeout: SEND_TIMEOUT,
+			recvTimeout: RECV_TIMEOUT,
+			checkTimeout: CHECK_TIMEOUT,
+			remoteLevel: "error",
+			clear: clear };
+	
+	for (var key in NAMES) self[key] = storage.getItem(itemType, key) || self[key];
+	for (var key in LOG_EVENTS) (function(key){
+		console[key+"Local"] = console[key];
+		console[key] = function(){
+			if (self.remoteURL && testLevel(self.remoteLevel, key)) {
+				if (testRegExp(self.remoteRegExp, arguments)) {
+					defer.apply(undefined,$.concat([key],arguments));
+				}
+			}
+			var result = undefined;
+			if (! self.localLevel || testLevel(self.localLevel, key)) {
+				if (isIE) for (var i=0; i<arguments.length; i++) {
+					if (typeof arguments[i]==="object") arguments[i] = $.formatJSON(arguments[i]);
+				}
+				if (testRegExp(self.localRegExp, arguments)) {
+					result = isIE9 ? console[key+"Local"]($.concat([],arguments)) : console[key+"Local"].apply(console,arguments);
+				}
+			}
+			checkLevel();
+			return result;
+		};
+	})(key);
+
+	// Publish API for console.akme() (getter) and console.akme({remoteURL:"",...}) setter for the singleton.
+	console.akme = function(map) {
+		if (map instanceof Object) {
+			$.copy(self, map);
+			for (key in NAMES) if (map[key] !== undefined) {
+				if (map[key] !== null) storage.setItem(itemType, key, self[key]);
+				else storage.removeItem(itemType, key);
+			}
+		}
+		return self;
+	};
+	
+	//
+	// helper functions
+	//
+	
+	function testLevel(level, key) {
+		return LOG_EVENTS[level] && LOG_EVENTS[key] >= LOG_EVENTS[level];
+	}
+	function testRegExp(re, args) {
+		return ! re || (args.length != 0 && re instanceof RegExp && re.test(args[0]));
+	}
+	
+	function setSelfAndStore(key, val) {
+		self[key] = val;
+		storage.setItem(itemType, key, val);
+	}
+	function removeSelfAndStore(key) {
+		delete self[key];
+		storage.removeItem(itemType, key);
+	}
+
+	/**
+	 * Remove related storage entries, including pending logs and last known contact timestamps with the server.
+	 */
+	function clear() {
+		delete self.remoteAttemptDate;
+		delete self.remoteSuccessDate;
+		storage.removeAll(itemType);
+	}
+	
+	/**
+	 * Check the X-Log-Level setting with the server.
+	 * Do this initially and then every 15 minutes or similarly configurable upon console events/calls.
+	 */
+	function checkLevel() {
+		var nowMillis = getTimeIfGreaterThanMillis(self.checkTimeout);
+		if (typeof nowMillis === "number") defer("check",nowMillis);
+	};
+	
+	/**
+	 * Returns new Date().getTime() if it's absolute difference is greater than the given millis, otherwise null.
+	 */
+	function getTimeIfGreaterThanMillis(millis) {
+		var nowMillis = new Date().getTime();
+		var remoteMillis = self.remoteSuccessDate || self.remoteAttemptDate;
+//console.infoLocal("getTimeIfGreaterThanMillis", nowMillis, remoteMillis, Math.abs(nowMillis-remoteMillis), self.checkTimeout)
+		return (console.remoteURL && (!remoteMillis || 
+				Math.abs(nowMillis-remoteMillis) >= millis)) ? nowMillis : null;
+	};
+	
+	/**
+	 * Store the console log event asynchronously and send later.
+	 */
+	function defer() {
+		var args = $.concat([],arguments);
+		var len = storage.getItem(itemType, "length") || 0;
+		storage.setItem(itemType, "length", ++len);
+		storage.setItemJSON(itemType, len-1, args);
+		if (!timer) timer = setTimeout(send, self.sendTimeout);
+	};
+	
+	/**
+	 * Send the stored console logs, typically called by a Timeout.
+	 */
+	function send() {
+		if (timer) { clearTimeout(timer); timer = null; }
+		if (!self.remoteURL) return;
+		var len = storage.getItem(itemType, "length") || 0;
+		var a = [], worstEvent = "log";
+		for (var i=0; i<len; i++) {
+			a[i] = storage.getItemJSON(itemType, i);
+			var itemEvent = a[i] && a[i].length > 0 ? a[i][0] : null;
+			if (itemEvent && LOG_EVENTS[itemEvent] > LOG_EVENTS[worstEvent]) worstEvent = itemEvent;
+		}
+		
+		var url = self.remoteURL+"."+worstEvent;
+		var user = self.remoteUser, pass = self.remotePass;
+		var tryCount = 0;
+		trySend();
+		
+		// Only provide Authorization once as a re-try, assuming *_token cookie will handle it thereafter.
+		function trySend() {
+			tryCount++;
+			// Give up if we tried more than twice.
+			if (tryCount > 2) return;
+			// MSIE10+HTML5 has Base64 btoa (to ASCII) and atob (to Binary) and use library to cover older browsers.
+			var headers = {
+				"Content-Type": MIME_TYPE,
+				"X-Log-Level": self.remoteLevel || "",
+				"X-Log-Local-Level": self.localLevel || ""};
+			if (tryCount==2 || !(self.remoteSuccessDate || self.remoteAttemptDate) ||
+					typeof getTimeIfGreaterThanMillis(self.checkTimeout) == "number") {
+				headers["Authorization"] = "Basic "+ btoa(user+":"+pass);
+			}
+			setSelfAndStore("remoteAttemptDate", new Date().getTime());
+			removeSelfAndStore("remoteSuccessDate");
+			//method, url, headers, content, /*function(headers,content)*/ callbackFnOrOb
+			var xhr = $.xhr.callAsync("POST", url, headers, a, tryReceive);
+			setTimeout(function(){ if (xhr) xhr.abort(); }, self.recvTimeout);
+			if (tryCount==1) {
+				// Only need to remove sent items on the first try.
+				storage.removeAll(itemType);
+				for (var key in NAMES) if (self[key] != null) storage.setItem(itemType, key, self[key]);
+				checkLevel(); // setTimeout check for later
+			}
+			
+			function tryReceive(headers,content) {
+				if (!headers.status || headers.status >= 400) {
+					if (tryCount==1 && headers.status == 401) trySend(); // Retry one time with Authorization.
+					return;
+				}
+				setSelfAndStore("remoteSuccessDate", new Date().getTime());
+				var logLevel = headers["X-Log-Level"];
+				if (logLevel && LOG_EVENTS[logLevel] >= LOG_EVENTS["log"]) {
+					setSelfAndStore("remoteLevel", logLevel);
+				}
+				logLevel = headers["X-Log-Local-Level"];
+				if (logLevel && LOG_EVENTS[logLevel] >= LOG_EVENTS["log"]) {
+					setSelfAndStore("localLevel", logLevel);
+				}
+				console.logLocal("POST "+ url +" response ", 
+					isIE?"\n"+$.formatJSON(headers):headers, 
+					isIE?"\n"+$.formatJSON(content):content);
+			};
+			
+		}
+			
+	};
+	
+})(akme,console);
---
+++ web/log/authenticate-shared.lua
@@ -0,0 +1,143 @@
+--[[
+ Handle authentication through a shared secret and message digest/hash.
+ e.g. where the :password is the UTC_millis:MD5_hash_of_(username:millis:secret).
+ username=j_keith.mashinter&j_password=1257629271921:157a7437ef7ce274754d24f0e655a225
+ fw_token=keith.mashinter:1257629271921:157a7437ef7ce274754d24f0e655a225
+]]
+
+local secret, secretPast = "foobar", nil
+local cookieName = "akme_token"
+local keepSecure = false
+local timeoutSecs = 65*60
+local refreshSecs = timeoutSecs/ 2
+
+local allowOrigins = {"http://localhost","https://localhost"}
+for i,v in ipairs(allowOrigins) do allowOrigins[v] = i end
+
+-- function handle(r) authenticate(r) end
+
+-- If including in another script.
+function authenticate(r)
+debug(r)
+	local username, password = parseAuth(r)
+    if not (username) and "POST" == r.method then
+    	args = r:parsebody()
+       	username = args["j_username"]
+       	password = args["j_password"]
+	end
+
+	local cookie, pos = r:getcookie(cookieName), -1
+	local hasCookie = cookie ~= nil
+	if not (username) and hasCookie then
+		username = r:unescape(cookie)
+		pos = username ~= nil and username:find(':') or -1
+		if pos >= 1 then
+			password = username:sub(pos+1)
+			username = username:sub(1,pos-1)
+		end
+	end
+
+	local contextPathOrRoot = string.len(r.context_prefix) ~= 0 and r.context_prefix or "/"
+
+	-- Lua os.time() is to seconds but typically timestamps are millis.
+  	local now, cooked = os.time(), (hasCookie and username ~= nil and password ~= nil)
+    local validated = validateRefer(r)
+
+	validated = validated and username ~= nil and password ~= nil
+   	-- Separate the timestamp from the given digest.
+	pos = validated and password:find(':') or -1
+    if not (pos >= 1) then
+    	validated = false
+    else
+		local time, hash = password:sub(1,pos-1), password:sub(pos+1)
+		local timediff = 0
+		-- Hex hashes should be compared case-insensitive.
+		timediff = math.abs( os.difftime(now,tonumber(time)/1000) )
+		validated = timeoutSecs == 0 or timediff < timeoutSecs
+
+		if (validated) then
+ 			validated = r:md5(username..":"..time..":"..secret) == hash
+			if (not validated and secretPast ~= nil and string.len(secretPast) ~= 0) then 
+				validated = r:md5(username..":"..time..":"..secretPast) == hash
+			end
+    	end
+    	if (validated) then
+			if (refreshSecs ~= 0 and timediff >= refreshSecs) then
+        		-- Re-stamp the password cookie every refreshMins if there is an overall timeoutMins.
+        		time = tostring(now*1000)
+        		hash = r:md5(username..":"..time..":"..secret)
+        		password = time ..":".. hash
+        		cooked = false
+			end
+			if (not cooked) then
+            	-- Set session/memory Cookies with username and password.
+				value = r:escape(username..":"..password)
+				r:setcookie({
+					  key = cookieName,
+					  value = value,
+					  domain = nil,
+					  path = r.context_prefix,
+					  secure = r.is_https and keepSecure,
+					  httponly = true
+					})
+			end
+	        -- Finally set the Principal username on the request.
+			r.user = username
+    	end
+	end
+
+    if (not validated) then 
+		if (hasCookie) then for i = 0,1 do 
+			-- Remove related Secure and non-Secure Cookies.
+			r:setcookie({
+				  key = cookieName,
+				  value = "",
+				  expires = 1, -- "Thu, 01 Jan 1970 00:00:01 GMT", 1 to delete, zero means session cookie
+				  path = r.context_prefix,
+				  secure = i==0,
+				  httponly = true
+				})
+		end end
+		--r.user = nil
+		return 401
+	else
+		return apache2.OK 
+	end
+	
+end
+
+
+-- Check the HTTP Referer to be a trusted source.
+function validateRefer(r)
+	if (allowOrigins ~= nil) then
+		local origin = r.headers_in["Referer"] -- HTTP spelling is Referer
+		if (origin ~= nil) then origin = origin:sub(1, string.find(origin, '/', 9)-1) end
+		if (origin == nil or not allowOrigins[origin]) then
+			return false
+		end
+	end
+	return true
+end
+
+
+-- Parse the Authorization header into a username and a password.
+function parseAuth(r)
+	local auth, user, pass = r.headers_in['Authorization'], nil, nil
+	if auth and auth:len() > 0 then
+		auth = r:base64_decode(auth:sub(7))
+		user, pass = auth:match("([^:]+)%:(%S*)")
+    end
+    return user, pass
+end
+
+-- r.notes["1"] = tostring( username ); debug(r)
+function debug(r)
+	local user = "user"
+	local millis = os.time()*1000 -- os.time() is only to seconds
+	msg = tostring(r.notes["1"]) ..'; user '.. user ..' pass '.. millis ..':'.. r:md5(user ..':'.. millis ..':'.. secret)
+	-- ... r:debug() r:info() r:notice() r:warn() r:err() ...
+	-- Apache defaults to notice level and up.
+	--r:notice(msg)
+	--r.headers_out["X-Debug"] = msg
+	r.err_headers_out["X-Debug"] = msg
+end
