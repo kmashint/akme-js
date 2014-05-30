be4c78007b36781316c3df479ab31dd00e77563a
kmashint <kmashint@yahoo.com>
2014-05-29 22:37:22 -0400

    remove more yui useless warnings

--- bin/yuicompress-js.js
+++ bin/yuicompress-js.js
@@ -1,11 +1,20 @@
+// Remove useless warnings.
 var ins = WScript.StdIn,
 	ous = WScript.StdOut,
-	line, i;
+	lineAry = [], i;
+//
 while (!ins.AtEndOfStream) {
-	line = ins.ReadLine();
-	if (/Try to use a single 'var' statement per scope/.test(line)) 
+	//ous.WriteLine(ins.ReadLine() ); continue;
+	lineAry.push( ins.ReadLine() );
+	if (/Try to use a single 'var' statement per scope/.test(lineAry[lineAry.length-1])) {
+		lineAry.length = 0;
 		skip(2);
-	else
-		ous.WriteLine(line);
+	} else if (lineAry.length > 1) {
+		write(lineAry);
+		lineAry.length = 0;
+	}
 }
-function skip(n) { for (i=0; i<n && !ins.AtEndOfStream; i++) ins.ReadLine(); }
+write(lineAry);
+//
+function skip(n) { for (var i=0; i<n && !ins.AtEndOfStream; i++) ins.ReadLine(); }
+function write(a) { for (var i=0; i<a.length; i++) ous.WriteLine(a[i]); }
